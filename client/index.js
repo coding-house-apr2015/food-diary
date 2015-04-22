e5ac08e7000a9e727e2afdbe0b16baa90e6225a6
Chyld Medford <chyld.medford@gmail.com>
2015-04-22 11:43:46 -0700

    user.save

--- client/index.js
+++ client/index.js
@@ -1,3 +1,16 @@
 'use strict';
 
-angular.module('poseidon', ['firebase']);
+angular.module('food-diary', ['firebase'])
+.run(['$rootScope', '$window', function($rootScope, $window){
+  $rootScope.fbRoot = new $window.Firebase('https://food-diary.firebaseio.com/');
+}])
+.controller('master', ['$scope', '$firebaseObject', function($scope, $firebaseObject){
+  var fbUser = $scope.fbRoot.child('user');
+  var afUser = $firebaseObject(fbUser);
+
+  $scope.user = afUser;
+
+  $scope.saveUser = function(){
+    $scope.user.$save();
+  };
+}]);
