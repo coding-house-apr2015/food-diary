57c4edf60472b80e42e7da26229a009eb9770c8f
Chyld Medford <chyld.medford@gmail.com>
2015-04-22 15:27:55 -0700

    remove

--- client/index.js
+++ client/index.js
@@ -4,20 +4,53 @@ angular.module('food-diary', ['firebase'])
 .run(['$rootScope', '$window', function($rootScope, $window){
   $rootScope.fbRoot = new $window.Firebase('https://food-diary.firebaseio.com/');
 }])
-.controller('master', ['$scope', '$firebaseObject', function($scope, $firebaseObject){
+.controller('master', ['$scope', '$firebaseObject', '$firebaseArray', function($scope, $firebaseObject, $firebaseArray){
   var fbUser = $scope.fbRoot.child('user');
   var afUser = $firebaseObject(fbUser);
-
   $scope.user = afUser;
+
+  var fbFoods = $scope.fbRoot.child('foods');
+  var afFoods = $firebaseArray(fbFoods);
+  $scope.foods = afFoods;
 
   $scope.saveUser = function(){
     $scope.user.$save();
+    $scope.isUserFormShown = false;
     computeBMI();
   };
 
   $scope.user.$loaded().then(function(){
     computeBMI();
   });
+
+  $scope.foods.$loaded().then(function(){
+    computeWeight();
+  });
+
+  $scope.addFood = function(){
+    var now = new Date();
+    $scope.food.date = now.getTime();
+    $scope.foods.$add($scope.food).then(function(){
+      computeWeight();
+    });
+  };
+
+  $scope.removeFood = function(food){
+    $scope.foods.$remove(food).then(function(){
+      computeWeight();
+    });
+  };
+
+  $scope.showUserForm = function(){
+    $scope.isUserFormShown = true;
+  };
+
+  function computeWeight(){
+    var totalCalories = $scope.foods.reduce(function(accumulator, food){
+      return accumulator + (food.calories * food.servings);
+    }, 0);
+    $scope.weightGained = totalCalories / 3500;
+  }
 
   function computeBMI(){
     var inches = $scope.user.unit === 'in' ? $scope.user.height : $scope.user.height * 0.393701;
